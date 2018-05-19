1626671a3442d60eda062d924be1b8a9f37e2692
NobiGo <nobigogle@gmail.com>
2018-05-19 11:31:29 +0800

    丰富wordCount

---
+++ src/main/scala/Tools/FileTools.java
@@ -0,0 +1,65 @@
+package Tools;
+
+import java.io.File;
+
+/**
+ * @author dx
+ * @date 2018/5/19 上午11:26
+ */
+public class FileTools {
+    /**
+     * 删除单个文件
+     *
+     * @param sPath 被删除文件path
+     * @return 删除成功返回true，否则返回false
+     */
+    public static boolean deleteFile(String sPath) {
+        boolean flag = false;
+        File file = new File(sPath);
+        // 路径为文件且不为空则进行删除
+        if (file.isFile() && file.exists()) {
+            file.delete();
+            flag = true;
+        }
+        return flag;
+    }
+
+    /**
+     * 删除目录以及目录下的文件
+     *
+     * @param sPath 被删除目录的路径
+     * @return 目录删除成功返回true，否则返回false
+     */
+    public static boolean deleteDirectory(String sPath) {
+        //如果sPath不以文件分隔符结尾，自动添加文件分隔符
+        if (!sPath.endsWith(File.separator)) {
+            sPath = sPath + File.separator;
+        }
+        File dirFile = new File(sPath);
+        //如果dir对应的文件不存在，或者不是一个目录，则退出
+        if (!dirFile.exists() || !dirFile.isDirectory()) {
+            return false;
+        }
+        boolean flag = true;
+        //删除文件夹下的所有文件(包括子目录)
+        File[] files = dirFile.listFiles();
+        for (int i = 0; i < files.length; i++) {
+            //删除子文件
+            if (files[i].isFile()) {
+                flag = deleteFile(files[i].getAbsolutePath());
+                if(!flag) break;
+            } //删除子目录
+            else {
+                flag = deleteDirectory(files[i].getAbsolutePath());
+                if(!flag) break;
+            }
+        }
+        if (!flag) return false;
+        //删除当前目录
+        if (dirFile.delete()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
--- src/main/scala/WordCountDemo/WordCount.scala
+++ src/main/scala/WordCountDemo/WordCount.scala
@@ -1,16 +1,27 @@
 package WordCountDemo
 
+import java.io.File
+
+import Tools.FileTools
+import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
 
 
 object WordCount {
   def main(args: Array[String]): Unit = {
-    val sparkConf:SparkConf = new SparkConf()
+    //新建配置文件环境
+    val sparkConf: SparkConf = new SparkConf()
     sparkConf.setAppName("WordCount").setMaster("local")
-    val sparkContext:SparkContext = new SparkContext(sparkConf)
-    val fileRdd  = sparkContext.textFile("/Users/dx/IdeaProjects/ScalaGo/README.md")
-    fileRdd.saveAsTextFile("/Users/dx/IdeaProjects/ScalaGo/out")
-    print(fileRdd.count())
+    //建立与SparkCluster的连接
+    val sparkContext: SparkContext = new SparkContext(sparkConf)
+    //读取输入的数据
+    val fileRdd = sparkContext.textFile("/Users/dx/IdeaProjects/ScalaGo/README.md")
+    //将它切分为一个个单词
+    val words: RDD[String] = fileRdd.flatMap(line => line.split(" "))
+    //将单词进行键值对转换并计数
+    val counts = words.map(word => (word, 1)).reduceByKey { case (x, y) => x + y }
+    FileTools.deleteDirectory("/Users/dx/IdeaProjects/ScalaGo/wordNum")
+    counts.saveAsTextFile("/Users/dx/IdeaProjects/ScalaGo/wordNum")
     sparkContext.stop()
   }
 }
